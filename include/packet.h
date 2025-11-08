a3e6764aa6e7ce0f1659b0b9d429b423dfa3d295
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-08 17:27:06 +0530

    Create packet header file

---
+++ include/packet.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<stdint.h>
+
+#pragma pack(push, 1)
+typedef struct frame
+{
+    uint8_t preamble; 
+    uint16_t seq_no;
+    uint16_t ack_no;
+    uint8_t data[512];
+    uint8_t crc;
+
+};
+#pragma pack(pop)
