ff8cb006d5fca1ddffefb9e839af6ef7b145506d
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-08 17:27:34 +0530

    Resolve array bug in temp crc

--- src/crc/crc.c
+++ src/crc/crc.c
@@ -13,14 +13,14 @@ void compute_crc(char* data, char* divisor, char* remainder){
     int len_data = strlen(data);
     int len_divisor = strlen(divisor);
 
-    char* temp[100];
+    char temp[100];
 
     //copy current data to temp
     strcpy(temp, data);
 
-    //copy n-1 divisor 0 bits to data
+    //append n-1 divisor 0 bits to data
     for(int i =0; i<len_divisor-1; i++){
-        strcpy(temp, '0');
+        strcat(temp, '0');
     }
 
     int n = strlen(temp);
