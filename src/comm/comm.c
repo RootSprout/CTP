9a3b38e389db6ec75079d6091ecf7f428c846a55
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-08 18:11:45 +0530

    Create receive_frame function

--- src/comm/comm.c
+++ src/comm/comm.c
@@ -11,6 +11,20 @@ void send_frame(int fd, frame_t *f){
     }
 }
 
+int receive_frame(int fd, frame_t *f){
+    uint8_t byte;
 
+    while(read(fd, &byte, 1) == 1){ //read preamble
+        if(byte == 0xAA){
+            f->preamble = byte;
+            read(fd, (uint8_t*)f+1, sizeof(*f)-1);
+            printf("File Read Successfully");
+            return 1;
+        }
+    }
+
+    printf("Error Reading file");
+    return 0;
+}
 
 
