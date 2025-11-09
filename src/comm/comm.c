3888de4c2cae6ea269d88fae676f69bff402cd43
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 18:25:22 +0530

    Bug include write in send_frame

--- src/comm/comm.c
+++ src/comm/comm.c
@@ -3,7 +3,7 @@
 #include "../../include/packet.h"
 
 void send_frame(int fd, frame_t *f){
-    ssize_t bytes_written = (fd, f, sizeof(*f));
+    ssize_t bytes_written = write(fd, f, sizeof(*f));
     if(bytes_written > 0){
         printf("Data sent successfully!");
     }else{
