68c93d2f5022e94dd40455d0f586a1161dfdd1c1
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 17:16:13 +0530

    Create string to bits function

--- src/crc/crc.c
+++ src/crc/crc.c
@@ -36,3 +36,17 @@ void compute_crc(char* data, char* divisor, char* remainder){
     strncpy(remainder, &temp[n- (len_divisor-1)], len_divisor-1);
     remainder[len_divisor - 1] = '\0';
 }
+
+void string_to_bits(char* message, char* output){
+    int k =0;
+    for(int i =0; i< strlen(message);i++){
+        for(int j =7; j>=0; j--){
+            unsigned char c = message[i]; //A->75
+
+            //1. left shift 1 by j to check the first bit and then sequentailly the next bits
+            output[k++] = ((c & 1<<j) )? '1' : '0';
+        }
+    }
+
+    output[k] = '\0';
+}
