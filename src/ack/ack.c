f2c23582ffab038150e5ef86b5103cef7e2b8c57
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 16:59:10 +0530

    Implement wait_for_ack function

--- src/ack/ack.c
+++ src/ack/ack.c
@@ -10,3 +10,17 @@ void send_ack(int fd, uint16_t seq_no, int success){
     memset(ack.data, 0, sizeof(ack.data));
     ack.crc = success ? 1: 0;
 }
+
+int wait_for_ack(int fd, uint16_t expected_seq) {
+    frame_t ack;
+    if (receive_frame(fd, &ack)) {
+        if (ack.ack_no == expected_seq && ack.crc == 1) {
+            printf("ACK for frame %d\n", expected_seq);
+            return 1;
+        } else {
+            printf("Resend frame %d\n", expected_seq);
+            return 0;
+        }
+    }
+    return 0;
+}
